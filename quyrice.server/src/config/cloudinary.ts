0e73608ab25f7216dd3ce2d59c3c695a8726e80f
nhanbernie <ttnhanbernie@gmail.com>
2025-05-07 13:56:38 +0700

    refactor: config cloudinary

---
+++ quyrice.server/src/config/cloudinary.ts
@@ -0,0 +1,9 @@
+import cloudinary from 'cloudinary'
+
+const configCloundinary = cloudinary.v2.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
+})
+
+export default configCloundinary
