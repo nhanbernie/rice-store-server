0d208996ddec25014e73c8a84abbd2552fd0fdc9
nhanbernie <ttnhanbernie@gmail.com>
2025-05-09 14:28:01 +0700

    delete: file or modules not use

---
+++ quyrice.server/src/models/entities/contact/contact.enity.ts
@@ -0,0 +1,27 @@
+import mongoose from 'mongoose'
+import { ContactItem, IContact } from 'models/types/contact/contact.type'
+
+const contactItemSchema = new mongoose.Schema<ContactItem>(
+  {
+    type: { type: String, required: true },
+    link: { type: String, required: true }
+  },
+  { _id: false }
+)
+
+const contactSchema = new mongoose.Schema<IContact>(
+  {
+    configId: {
+      type: String,
+      default: 'singleton',
+      unique: true,
+      select: false
+    },
+    contacts: [contactItemSchema]
+  },
+  { timestamps: true }
+)
+
+const Contact = mongoose.model<IContact>('Contact', contactSchema, 'contacts')
+
+export default Contact
