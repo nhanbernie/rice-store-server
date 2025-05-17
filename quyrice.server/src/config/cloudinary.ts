54b82f9c7345c2c90256d019ac72ac201538bb2d
nhanbernie <ttnhanbernie@gmail.com>
2025-05-17 16:31:42 +0700

    config(cloud): add cloudinary configuration

--- quyrice.server/src/config/cloudinary.ts
+++ quyrice.server/src/config/cloudinary.ts
@@ -1,9 +1,9 @@
-import cloudinary from 'cloudinary'
+import { v2 as cloudinary } from 'cloudinary';
 
-const configCloundinary = cloudinary.v2.config({
+cloudinary.config({
   cloud_name: process.env.CLOUDINARY_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET
-})
+});
 
-export default configCloundinary
+export default cloudinary;
